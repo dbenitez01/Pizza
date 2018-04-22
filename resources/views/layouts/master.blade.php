09272b432c93487465c543530f4fa9dfc9bb5402
kagron <kagron12@gmail.com>
2018-04-22 17:51:22 -0500

    Revert "ldjfalsd"
    This reverts commit 464d4118715b1d9db3f58cadef64373833baca49.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -38,7 +38,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li><a class="nav-link" href="{{ route('orders.create') }}">Menu</a></li>
+                        <li><a class="nav-link" href="#">Menu</a></li>
+                        <li><a class="nav-link" href="{{ route('orders.create') }}">Order</a></li>
                         <li><a class="nav-link" href="#">Contact</a></li>
                         @auth
                           @if (Auth::user()->is_admin)
